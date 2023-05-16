c9176f6a99b14fd931750fdc0ca5fbcd721169f3
Peter Bajurny <bajurny@umn.edu>
2023-05-16 13:14:20 -0500

    Export more objects

--- grouper_python/objects/__init__.py
+++ grouper_python/objects/__init__.py
@@ -1,9 +1,21 @@
 """grouper_python.objects, Classes for the grouper_python package."""
 
-from .group import Group
+from .group import Group, CreateGroup
 from .person import Person
-from .stem import Stem
+from .stem import Stem, CreateStem
 from .subject import Subject
 from .privilege import Privilege
+from .membership import Membership, MemberType, MembershipType
 
-__all__ = ["Group", "Person", "Stem", "Subject", "Privilege"]
+__all__ = [
+    "Group",
+    "Person",
+    "Stem",
+    "Subject",
+    "Privilege",
+    "CreateGroup",
+    "CreateStem",
+    "Membership",
+    "MemberType",
+    "MembershipType",
+]
